d22609c2d74899cf1b715e22c03b2e8994138a41
Drew Benson <drew@adbenson.net>
2021-05-06 01:30:39 -0400

    Initial commit

---
+++ src/infinihedron/Infinihedron.java
@@ -0,0 +1,26 @@
+package infinihedron;
+
+import processing.core.PApplet;
+
+public class Infinihedron extends PApplet {
+
+    // The argument passed to main must match the class name
+    public static void main(String[] args) {
+        PApplet.main("infinihedron.Infinihedron");
+    }
+
+    // method used only for setting the size of the window
+    public void settings(){
+        size(1200, 1200);
+    }
+
+    // identical use to setup in Processing IDE except for size()
+    public void setup(){
+        
+    }
+
+    // identical use to draw in Prcessing IDE
+    public void draw(){
+
+    }
+}
